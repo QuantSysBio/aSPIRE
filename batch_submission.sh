cc2134e1a4a7d6156a2b9d32df28fa6bd36bd8d3
hroetsc <hanna.roetschke@mpinat.mpg.de>
2024-09-17 10:15:29 +0100

    update batch_submission.sh

--- batch_submission.sh
+++ batch_submission.sh
@@ -3,9 +3,5 @@
 for f in data/config_files/*.yaml; do
 	echo "$f"
 	cp -rf $f data/config.yaml
-<<<<<<< HEAD
 	snakemake --use-conda --cores all -R create_input
-=======
-	snakemake --use-conda --cores all
->>>>>>> v.1/v.1
 done
